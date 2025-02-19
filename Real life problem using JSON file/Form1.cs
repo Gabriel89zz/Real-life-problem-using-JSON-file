836a9a997320b2c51275e5b85f7ab1189afca136
Gabriel Torres <elcomparosh97@gmail.com>
2025-02-18 22:31:23 -0600

    Agregar archivos de proyecto.

---
+++ Real life problem using JSON file/Form1.cs
@@ -0,0 +1,10 @@
+namespace Real_life_problem_using_JSON_file
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
